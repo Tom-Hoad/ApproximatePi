4c659559aa18b8b7e7f82ccff72e1878504da82e
tom-hoad <tdh1g19@soton.ac.uk>
2020-08-14 12:18:38 +0100

    Detects if the dot is in the circle or not - shown with colour

--- src/ApproximatePi.java
+++ src/ApproximatePi.java
@@ -9,31 +9,47 @@ import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
+import java.util.Random;
+
 public class ApproximatePi extends Application {
+    private final int radius = 200;
+    private final Random rand = new Random();
+
     @Override
     public void start(Stage stage) {
         stage.setTitle("ApproximatePi");
 
         // Creates the circle.
         Circle circle = new Circle();
-        circle.setRadius(200);
-        circle.setLayoutX(200);
-        circle.setLayoutY(200);
+        circle.setRadius(radius);
+        circle.setLayoutX(radius);
+        circle.setLayoutY(radius);
         circle.setStroke(Color.WHITE);
 
         Pane layout = new Pane();
         layout.getChildren().addAll(circle);
 
-        // Creates random dots.
+        // Creates random dots indefinitely.
         Timeline timeline = new Timeline(
                 new KeyFrame(
                         Duration.millis(10),
                         event -> {
                             Circle dot = new Circle();
                             dot.setRadius(0);
-                            dot.setLayoutX(Math.random() * 400);
-                            dot.setLayoutY(Math.random() * 400);
-                            dot.setStroke(Color.WHITE);
+
+                            int x = rand.nextInt(2 * radius);
+                            int y = rand.nextInt(2 * radius);
+                            double d = Math.sqrt(((x - 200) * (x - 200)) + ((y - 200) * (y - 200)));
+                            System.out.println("X: " + x + " Y: " + y + " D: " + d);
+
+                            dot.setLayoutX(x);
+                            dot.setLayoutY(y);
+
+                            if (d < radius) {
+                                dot.setStroke(Color.GREEN);
+                            } else {
+                                dot.setStroke(Color.WHITE);
+                            }
 
                             layout.getChildren().add(dot);
                         }
@@ -42,7 +58,7 @@ public class ApproximatePi extends Application {
         timeline.setCycleCount(Animation.INDEFINITE);
         timeline.play();
 
-        Scene scene = new Scene(layout, 400, 400, Color.BLACK);
+        Scene scene = new Scene(layout, 2 * radius, 2 * radius, Color.BLACK);
         stage.setScene(scene);
         stage.show();
     }
